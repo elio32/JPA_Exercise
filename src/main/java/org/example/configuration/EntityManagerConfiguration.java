7dabfe5b2278c54cbb2ebe4138a50a9cbf8090e1
elio32 <eliosinaj@yahoo.com>
2023-04-12 16:57:22 +0200

    JPA

--- src/main/java/org/example/configuration/EntityManagerConfiguration.java
+++ src/main/java/org/example/configuration/EntityManagerConfiguration.java
@@ -6,7 +6,7 @@ import javax.persistence.Persistence;
 
  public final class EntityManagerConfiguration {
 
-        private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("jpa-intro");
+        private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("jpa_exercise_db");
         private static final EntityManager ENTITY_MANAGER = FACTORY.createEntityManager();
         private EntityManagerConfiguration() {}
         public static EntityManager getEntityManager() {
--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -1,18 +1,20 @@
 package org.example.main;
 
-import org.example.model.entity.User;
+import org.example.model.dto.UserDTO;
 import org.example.service.impl.UserServiceImpl;
 
 import javax.persistence.PersistenceException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
       try {
           UserServiceImpl userService = new UserServiceImpl();
-          userService.loadAllUsers();
-          userService.findUserById(123l);
-
+          List<UserDTO> users = userService.loadAllUsers();
+          for (UserDTO user : users){
+              System.out.println(user);
+          }
       }catch (ExceptionInInitializerError |PersistenceException e){
           System.out.println(e.getMessage());
       }
